fa2788f68ae0cb9cf92c825f07c1ece29663d7e1
zachsusee <susee@wustl.edu>
2025-07-09 11:32:49 -0500

    created spring project

---
+++ climblog/src/main/java/com/zach/climblog/ClimblogApplication.java
@@ -0,0 +1,13 @@
+package com.zach.climblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimblogApplication.class, args);
+	}
+
+}
---
+++ climblog/src/test/java/com/zach/climblog/ClimblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zach.climblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
